b6b80cb98ed6dbff7592a5f4334ad9a0322194a6
Ikeshala <ikeshala.peiris@gmail.com>
2023-12-17 14:53:14 +0530

    Implemented the Item section, added a dummy modal, and initiated the pop-up view to add items. Also, restructured the entire file system in a standardized way.

--- asset/js/report.js
+++ asset/js/report.js
@@ -1,11 +1,15 @@
 //Get the data-category attribute value from the clicked link
 $(document).ready(function () {
     $(".report-table .row").hide();
+    $(".report-table .row.annual-report").show();
+    $(".report-link[data-category='annual-report']").addClass("active");
 
     $(".report-link").on("click", function (e) {
         e.preventDefault();
         var category = $(this).data("category");
         $(".report-table .row").hide();
         $(".report-table .row." + category).show();
+        $(".report-link").removeClass("active");
+        $(this).addClass("active");
     });
 });
